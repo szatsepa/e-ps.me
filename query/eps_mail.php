a67894cbc70aa71580a741e96461f9c4d4440332
szatsepa <arcady.1254@gmail.com>
2012-03-10 19:12:33 +0200

    bags# => mail->address->responce

--- query/eps_mail.php
+++ query/eps_mail.php
@@ -3,7 +3,7 @@
 /*
  * created by arcady.1254@gmail.com 5/3/2012
  */
-print_r($attributes);
+//print_r($attributes);
 
 $email = $attributes[send];
 
@@ -21,20 +21,30 @@ $eps = $row[eps_cod];
 
 $id = $row[id]; 
 
-            $message ="Здравствуйте товарисчЪ! Ваш валшебный ключ - $eps.\r\n Добавлен новый емейл - $email\r\nC уважением. Администрация. ";              
+            $message ="Здравствуйте товарисчЪ! Ваш валшебный ключ - $eps.\r\n Добавлен новый емейл - $recipe\r\nC уважением. Администрация. ";              
              
-             $headers = 'From: administrator@'. $host. "\r\n";
+            $headers = 'From: administrator@'. $host. "\r\n";
             
             $headers  .= 'MIME-Version: 1.0' . "\r\n";
             
             $headers .= 'Content-type: text/plain; charset=utf-8' . "\r\n";
             
-            mail($email, 'Ваш валшебный ключ', $message, $headers);
+             mail($email, 'Ваш валшебный ключ', $message, $headers);
             
  $query = "INSERT INTO eps_recipient (sender_id, recipient) VALUES ($id, '$recipe')";
  
  mysql_query($query) or die($query);
  
+            $message ="Здравствуйте товарисчЪ! Некто, (емайл - $email) прислал Вам валшебный ключ - $eps.\r\nC уважением. Администрация. ";              
+             
+            $headers = 'From: '. $email. "\r\n";
+             
+            $headers  .= 'MIME-Version: 1.0' . "\r\n";
+            
+            $headers .= 'Content-type: text/plain; charset=utf-8' . "\r\n";
+            
+            mail($recipe, 'Ваш валшебный ключ', $message, $headers);
+           
  header("location:index.php?act=main");
                 
                  ?>
