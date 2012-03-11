adad08b02a7c59858f51864410349fc20d63e655
szatsepa <arcady.1254@gmail.com>
2012-03-11 16:43:20 +0200

    cursor:pointer

--- main/main.php
+++ main/main.php
@@ -8,6 +8,8 @@ $eps = $user->data[eps];
 $email = $user->data[email];
 
 $eps_array = str_split($eps);
+
+
 ?>
 <div class="envelope_base">
     <div class="envelope">
@@ -17,27 +19,27 @@ $eps_array = str_split($eps);
         </div>
       <form id="cover">
         <div class="recipe">
-            <input type="text" name="recipe" size="22" required/>
+            <input type="text" name="recipe" size="22"  required placeholder="Адрес получателя"/>
         </div>
         <div class="send">
-            <input type="text" name="send" size="22" value="<?php echo $email;?>" required/>
+            <input type="text" name="send" size="22" value="<?php echo $email;?>"  required placeholder="Ваш адрес"/>
         </div>
             <div class="submit_cover">
                 <input type="image" src="http://e-ps.me/images/submit_cover.jpg"  alt="BUTTON" onclick="javascript:_mySend('cover',<?php echo $_SESSION[auth];?>);"/>
             </div>
     </form>
-            <div class="reg_index" style="margin-top: 100px;">
+            <?php if($eps){
+                     $eps = '"'.$eps.'"';
+            }?>
+            <div class="reg_index" style="margin-top: 100px;"  title='Скопировать код в буфер обмена' onmouseover="this.style='cursor:pointer'" onmouseout="this.style='cursor:default'">
             
             <?php 
-            if($eps){
-                     $eps = '"'.$eps.'"';
-            }
+            
             for($i=0;$i<8;$i++){
                 $ml = ($i*45)."px";
                  
                  if($eps){
-//                     $eps = '"'.$eps.'"';
-                     echo "<div class='r_index_0' style='margin-left: ".$ml.";'><input type='image' src='http://".$host."/images/symbols/".  strtolower($eps_array[$i]).".png' title='Скопировать код в буфер обмена' onclick='javascript:_copyToclipboard($eps)'/>";//
+                     echo "<div class='r_index_0' style='margin-left: ".$ml.";'><input type='image' src='http://".$host."/images/symbols/".  strtolower($eps_array[$i]).".png' onclick='javascript:_copyToclipboard($eps);' />";//
                  }else{
                      echo "<div class='r_index_0' style='margin-left: ".$ml.";'><img src='http://".$host."/images/symbols/index_plase.jpg'/>";
                  }
